0b8f32f638dcfdfd51569426da59037449171e0e
Zohaib Ahmed <zohaib.ahmed@bloomrix.com>
2021-04-10 13:53:11 +0500

    Wrote seeder for Post model.

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->title,
+        'excerpt' => $faker->sentence,
+        'body' => $faker->paragraph,
+        'user_id' => 1,
+    ];
+});
