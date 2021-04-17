4fe65b4f2f7c8775ab38bb2fc8b9c7ad33edecb9
faizanmirza1133 <faizan.mirza@bloomrix.com>
2021-04-17 17:30:32 +0500

    no complete yet

--- app/Http/Livewire/Post/Show.php
+++ app/Http/Livewire/Post/Show.php
@@ -7,24 +7,19 @@ use Faker\Guesser\Name;
 use http\Env\Request;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\withPagination;
 
 class Show extends Component
 {
-    public function index(Request $request)
-    {
-        $post = Post::where([
-            ['name', '!=', Null],
-            [function ($query) use ($request){
-                if (($term = $request->term)){
-                    $query->orWhere('title', 'Like', '%'. $term . '%')->get();
-                }
-            }]
-        ])->orderBy("id", "desc");
+    use withPagination;
+    protected $paginationTheme = 'bootstrap';
+    public $search;
 
-    }
     public function render(Post $post)
     {
-        $posts = Auth::user()->posts()->paginate(5);
+        $posts = Auth::user()->posts()->when($this->search, function($query , $value){
+            $query->where('title', 'LIKE', '%' . $value .  '%')->orWhere('excerpt', 'LIKE', '%' . $value . '%')->orWhere->orderBy('id','DESC') ;
+        })->paginate(5);
         return view('livewire.post.show', compact('posts'));
     }
 }
