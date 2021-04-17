45f4d8cf006de9f19e494ec62c3e4368a56c85f1
Ahmad Saeed <ahmad.saeed@bloomrix.com>
2021-04-17 14:42:32 +0500

    chnage

---
+++ resources/views/converter.blade.php
@@ -0,0 +1,19 @@
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    {{-- @livewire('counter') --}}
+    {{-- @livewire('posts') --}}
+    {{-- @livewire('datatable') --}}
+    <div class="card">
+        <div class="card-header">
+            Converter
+        </div>
+        <div class="card-body">
+
+        </div>
+
+    </div>
+</div>
+@endsection
