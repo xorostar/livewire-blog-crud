109a3fb739868e6853b5669aa9f48c62528fc28e
Ahmad Saeed <ahmad.saeed@bloomrix.com>
2021-04-17 17:01:36 +0500

    make area converte with alpine js

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::view('/converter', 'converter')->name('converter');
+Route::get('/converter', fn () => view('converter'))->name('converter');
